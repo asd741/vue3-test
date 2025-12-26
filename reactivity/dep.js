cb069146db158dc28ece2b265ca63aac04d3f56c
asd741 <a22833593@gmail.com>
2025-12-26 15:39:48 +0800

    .

--- reactivity/dep.js
+++ reactivity/dep.js
@@ -1,1 +1,24 @@
 export const targetMap = new WeakMap();
+export const track = (target, key) => {
+  if (activeEffect) {
+    let depsMap = targetMap.get(target);
+    if (!depsMap) {
+      depsMap = new Map();
+      targetMap.set(target, depsMap);
+    }
+    let dep = depsMap.get(key);
+    if (!dep) {
+      dep = new Set();
+      depsMap.set(key, dep);
+    }
+    dep.add(activeEffect);
+  }
+};
+export const trigger = (target, key) => {
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  const dep = depsMap.get(key);
+  if (dep) {
+    dep.forEach(effect => effect.run());
+  }
+};
--- reactivity/reactive.js
+++ reactivity/reactive.js
@@ -1,33 +1,17 @@
 import { activeEffect } from "./effect";
-import { targetMap } from "./dep";
+import { targetMap, track, trigger } from "./dep";
 
 export function reactive(target) {
   return new Proxy(target, {
     get(target, key) {
       if (activeEffect) {
-        let depsMap = targetMap.get(target);
-        if (!depsMap) {
-          depsMap = new Map();
-          targetMap.set(target, depsMap);
-        }
-        let dep = depsMap.get(key);
-        if (!dep) {
-          dep = new Set();
-          depsMap.set(key, dep);
-        }
-        dep.add(activeEffect);
+        track(target, key);
       }
       return target[key];
     },
     set(target, key, value) {
       target[key] = value;
-      const depsMap = targetMap.get(target);
-      if (depsMap) {
-        const dep = depsMap.get(key);
-        if (dep) {
-          dep.forEach(effect => effect.run());
-        }
-      }
+      trigger(target, key);
       return true;
     },
   });
